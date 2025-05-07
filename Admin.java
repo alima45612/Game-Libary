a49bf2d0126aa31bdc2f4958f421d39befb2189f
alima <tamannaalima@gmail.com>
2025-05-07 12:07:24 +0100

    admin menu created

--- Admin.java
+++ Admin.java
@@ -8,5 +8,14 @@ public class Admin extends User {
         super(name, email, phonenumber);
     }
 
+    @Override
+    public void menu() {
+        System.out.println("1. View Games");
+        System.out.println("2. Add Games");
+        System.out.println("3. Delete Games");
+        System.out.println("4. Search Games");
+        System.out.println("5. Delete All Data");
+        System.out.println("6. Exit");
+    }
     
 }
