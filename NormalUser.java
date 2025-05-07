43433a8ba3d767846331a0a13f037bdc33d93731
alima <tamannaalima@gmail.com>
2025-05-07 12:07:54 +0100

    added menu to normal user file

--- NormalUser.java
+++ NormalUser.java
@@ -8,4 +8,15 @@ public class NormalUser extends User{
     public NormalUser(String name, String email, String phonenumber) {
         super(name, email, phonenumber);
     }
+
+    @Override
+    public void menu() {
+        System.out.println("1. View Games");
+        System.out.println("2. Add Games");
+        System.out.println("3. Delete Games");
+        System.out.println("4. Search Games");
+        System.out.println("5. Delete All Data");
+        System.out.println("6. Exit");
+    }
+
 }
